6b566364e79cd63def4f4d8e1a577bd57062ea0b
Luke Smith <lukesmudgesmith@gmail.com>
2016-06-16 12:24:43 +0100

    added new Zone class as well as Zone csv data file

---
+++ src/OrderForm.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by Luke on 15/06/2016.
+  */
+class OrderForm(oID:String, cID:String, fn:String, sn:String, loc:String, stat:String, sid:String,){
+  var orderID:String = oID
+  var customerID:String = cID
+  var firstName:String = fn
+  var surname:String = sn
+  var location:String = loc
+  var status:String = stat
+  var staffID:String = sid
+}
---
+++ src/Zone.scala
@@ -0,0 +1,20 @@
+/**
+  * Created by Luke on 16/06/2016.
+  */
+
+//Zone class represents the different zones in a warehouse where stock is stored
+//Zone name, distTOA, B, C, D, E, F, G, H, I, J
+class Zone (zn:String, a:Double, b:Double, c:Double, d:Double, e:Double,
+            f:Double, g:Double, h:Double, i:Double, j:Double) {
+  var zoneName:String = zn
+  var distToA:Double = a
+  var distToB:Double = b
+  var distToC:Double = c
+  var distToD:Double = d
+  var distToE:Double = e
+  var distToF:Double = f
+  var distToG:Double = g
+  var distToH:Double = h
+  var distToI:Double = i
+  var distToJ:Double = j
+}
