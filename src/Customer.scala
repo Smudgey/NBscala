04edc106527b733f356134b84c54b17fc00c0364
Luke Smith <lukesmudgesmith@gmail.com>
2016-06-16 12:25:35 +0100

    added new files for Order class, Customer class and customer csv

---
+++ src/Customer.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by Luke on 16/06/2016.
+  */
+//Stores Customer information: customer ID, fname, sname, addressLine1, county, post code
+class Customer (cid:String, fn:String, sn:String, AL1:String, cnty:String, pc:String) {
+  var customerID:String = cid
+  var firstName:String = fn
+  var surname:String = sn
+  var addressLine1 = AL1
+  var county = cnty
+  var postCode = pc
+}
---
+++ src/Order.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by Luke on 16/06/2016.
+  */
+//Contains the detail of an order, contained within OrderForm
+class Order (pid:String, pn:String, q:Int) {
+  var productID:String = pid
+  var productName:String = pn
+  var quantity:Int = q
+}
