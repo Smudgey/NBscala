2d47bbfeb637c00fbdc4eaa7c59d51e23ffe7c1f
Luke Smith <lukesmudgesmith@gmail.com>
2016-06-24 10:27:23 +0100

    Renamed files and edited .gitignore file

--- src/Order.scala
+++ src/Order.scala
@@ -1,5 +1,22 @@
 /**
-  * Created by Luke on 16/06/2016.
+  * Created by Luke on 15/06/2016.
   */
-//Contains the detail of an order, contained within OrderForm
-case class Order (productID:String, productName:String, quantity:Int) {}
+case class OrderForm(orderID:String, customerID:String, firstName:String, surName:String, location:String, status:String, staffID:String){}
+
+object OrderForm {
+  var orders = Set(Product("01", "01", "tom", "Tommy", "Bristol", "Active", "01"))
+  //possibly Seq instead
+  def findByOrderId(orderId:String) = orderForms.find(_.orderID == orderId)
+
+  def addOrder(productid: String, productname: String, quantity: Int): Unit = {
+    orderForms = orderForms + new Product(productid, productname, quantity)
+    orderList = orderList :+ new Product(productid, productname, quantity)
+    orderCount += 1
+  }
+
+  def getOrder(index: Int): String = {
+    orderList(index).productID + "," + orderList(index).productName + "," + orderList(index).quantity
+  }
+}
+
+Product.findByOrderId(getUserInput())
---
+++ src/Product.scala
@@ -0,0 +1,5 @@
+/**
+  * Created by Luke on 16/06/2016.
+  */
+//Contains the detail of an order, contained within OrderForm
+case class Product(productID:String, productName:String, quantity:Int) {}
